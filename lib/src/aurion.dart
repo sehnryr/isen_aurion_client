9721b133829b7fec5db983fcaaa9b01ae01a3c3b
sehnryr <youn@melois.dev>
2022-09-06 08:24:24 +0200

    make date boundaries better

--- lib/src/aurion.dart
+++ lib/src/aurion.dart
@@ -23,14 +23,16 @@ class IsenAurionClient {
 
   DateTime get defaultStart {
     var now = DateTime.now();
-    return now.subtract(Duration(days: 1 * 7 + now.weekday));
+    var today = DateTime(now.year, now.month, now.day, 0, 0, 0);
+    return today.subtract(Duration(days: 1 * 7 + now.weekday));
   }
 
   DateTime get defaultEnd {
     var now = DateTime.now();
     var endOfYear = DateTime(now.year, 7, 31, 23, 59, 59);
     bool newSchoolYear = now.isAfter(endOfYear);
-    return DateTime(now.year + (newSchoolYear ? 1 : 0), 7, 31, 23, 59, 59);
+    var end = DateTime(now.year + (newSchoolYear ? 1 : 0), 7, 31, 23, 59, 59);
+    return end.subtract(Duration(days: end.weekday + 1));
   }
 
   // The whole group tree
