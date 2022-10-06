dead5afbe0fbd1a114916946e469b48e808d4f0f
sehnryr <youn@melois.dev>
2022-10-06 20:48:19 +0200

    feat: add base event model type

--- lib/src/event.dart
+++ lib/src/event.dart
@@ -1,1 +1,36 @@
+/// Possible event types
+enum EventType {
+  course,
+  exam,
+  leave,
+  meeting,
+  practicalWork,
+  supervisedWork,
+  undefined,
+}
 
+/// Schedule event class.
+class Event {
+  final int id;
+  final EventType type;
+  final DateTime start;
+  final DateTime end;
+  final String room;
+  final String subject;
+  final String chapter;
+  final List<String> participants;
+
+  const Event({
+    required this.id,
+    required this.type,
+    required this.start,
+    required this.end,
+    required this.room,
+    required this.subject,
+    required this.chapter,
+    required this.participants,
+  });
+
+  @override
+  String toString() => subject;
+}
