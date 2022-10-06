575c1211183af553ac5ff9a7ed0cc2a4664b8faf
sehnryr <youn@melois.dev>
2022-10-07 00:52:46 +0200

    fix(event): make datetime saved as timestamp

--- lib/src/event.dart
+++ lib/src/event.dart
@@ -25,7 +25,9 @@ enum EventType {
 class Event {
   final int id;
   final EventType type;
+  @JsonKey(fromJson: _dateFromJson, toJson: _dateToJson)
   final DateTime start;
+  @JsonKey(fromJson: _dateFromJson, toJson: _dateToJson)
   final DateTime end;
   final String room;
   final String subject;
@@ -54,4 +56,8 @@ class Event {
 
   factory Event.fromJson(Map<String, dynamic> json) => _$EventFromJson(json);
   Map<String, dynamic> toJson() => _$EventToJson(this);
+
+  static DateTime _dateFromJson(int timestamp) =>
+      DateTime.fromMillisecondsSinceEpoch(timestamp);
+  static int _dateToJson(DateTime dateTime) => dateTime.millisecondsSinceEpoch;
 }
